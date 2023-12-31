c69c0b4e3e464552abe8f1c89e013e9ea38bc0d5
shyamkumargithub <55012474+shyamkumargithub@users.noreply.github.com>
2023-12-31 08:28:24 +0530

    Add files via upload

---
+++ craco.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  eslint: {
+    enable: false
+  },
+  style: {
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')]
+    }
+  }
+};
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
+
